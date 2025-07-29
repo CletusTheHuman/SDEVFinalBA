ddd9bcf28c622ec7002613d431443f6a381b1e27
B Archer <Barcher802@gmail.com>
2025-07-29 14:16:34 -0400

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -14,14 +14,64 @@ root.title('SDEV 120 Tic-Tac-Toe')
 clicked = True
 count = 0
 
+# Start the game over
+def reset():
+    global b1, b2, b3, b4, b5, b6, b7, b8, b9
+    global clicked, count
+    clicked = True
+    count = 0
+    # Build buttons
+    b1 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b1))
+    b2 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b2))
+    b3 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b3))
+
+    b4 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b4))
+    b5 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b5))
+    b6 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b6))
+
+    b7 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b7))
+    b8 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b8))
+    b9 = Button(root, text=" ", font=("Helvetica", 20), height=3, width=6, bg="SystemButtonFace",
+                command=lambda: b_click(b9))
+
+    # Grid our buttons to the screen
+    b1.grid(row=0, column=0)
+    b2.grid(row=0, column=1)
+    b3.grid(row=0, column=2)
+
+    b4.grid(row=1, column=0)
+    b5.grid(row=1, column=1)
+    b6.grid(row=1, column=2)
+
+    b7.grid(row=2, column=0)
+    b8.grid(row=2, column=1)
+    b9.grid(row=2, column=2)
 # disable all the buttons
 def disable_all_buttons():
-    pass
+    b1.config(state=DISABLED)
+    b2.config(state=DISABLED)
+    b3.config(state=DISABLED)
+    b4.config(state=DISABLED)
+    b5.config(state=DISABLED)
+    b6.config(state=DISABLED)
+    b7.config(state=DISABLED)
+    b8.config(state=DISABLED)
+    b9.config(state=DISABLED)
+
 
 # Check to see if someone won
 def checkIfWon():
     global winner
     winner = False
+
 #CHECK X WINS
     # winner across
     if b1["text"] == "X" and b2["text"] == "X" and b3["text"] == "X":
@@ -86,67 +136,73 @@ def checkIfWon():
         disable_all_buttons()
 
 # CHECK 0 WINS
-        # winner across
-        if b1["text"] == "X" and b2["text"] == "O" and b3["text"] == "O":
-            b1.config(bg="green")
-            b2.config(bg="green")
-            b3.config(bg="green")
-            winner = True
-            messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
-            disable_all_buttons()
-        elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
-            b4.config(bg="green")
-            b5.config(bg="green")
-            b6.config(bg="green")
-            winner = True
-            messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
-            disable_all_buttons()
-        elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
-            b7.config(bg="green")
-            b8.config(bg="green")
-            b9.config(bg="green")
-            winner = True
-            messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
-            disable_all_buttons()
+    # Winner Across
+    if b1["text"] == "X" and b2["text"] == "O" and b3["text"] == "O":
+        b1.config(bg="green")
+        b2.config(bg="green")
+        b3.config(bg="green")
+        winner = True
+        messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
+        disable_all_buttons()
 
-        # Winner vertical
-        elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
-            b1.config(bg="green")
-            b4.config(bg="green")
-            b7.config(bg="green")
-            winner = True
-            messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
-            disable_all_buttons()
-        elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
-            b2.config(bg="green")
-            b5.config(bg="green")
-            b8.config(bg="green")
-            winner = True
-            messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
-            disable_all_buttons()
-        elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
-            b3.config(bg="green")
-            b6.config(bg="green")
-            b9.config(bg="green")
-            winner = True
-            messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
-            disable_all_buttons()
+    elif b4["text"] == "O" and b5["text"] == "O" and b6["text"] == "O":
+        b4.config(bg="green")
+        b5.config(bg="green")
+        b6.config(bg="green")
+        winner = True
+        messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
+        disable_all_buttons()
 
-        # Winner Diagnol
-        elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
-            b1.config(bg="green")
-            b5.config(bg="green")
-            b9.config(bg="green")
-            winner = True
-            messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
-            disable_all_buttons()
-        elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
-            b1.config(bg="green")
-            b4.config(bg="green")
-            b7.config(bg="green")
-            winner = True
-            messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
-            disable_all_buttons()
+    elif b7["text"] == "O" and b8["text"] == "O" and b9["text"] == "O":
+        b7.config(bg="green")
+        b8.config(bg="green")
+        b9.config(bg="green")
+        winner = True
+        messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
+        disable_all_buttons()
+
+    # Winner vertical
+    elif b1["text"] == "O" and b4["text"] == "O" and b7["text"] == "O":
+        b1.config(bg="green")
+        b4.config(bg="green")
+        b7.config(bg="green")
+        winner = True
+        messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
+        disable_all_buttons()
+
+    elif b2["text"] == "O" and b5["text"] == "O" and b8["text"] == "O":
+        b2.config(bg="green")
+        b5.config(bg="green")
+        b8.config(bg="green")
+        winner = True
+        messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
+        disable_all_buttons()
+
+    elif b3["text"] == "O" and b6["text"] == "O" and b9["text"] == "O":
+        b3.config(bg="green")
+        b6.config(bg="green")
+        b9.config(bg="green")
+        winner = True
+        messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
+        disable_all_buttons()
+
+    # Winner Diagnol
+    elif b1["text"] == "O" and b5["text"] == "O" and b9["text"] == "O":
+        b1.config(bg="green")
+        b5.config(bg="green")
+        b9.config(bg="green")
+        winner = True
+        messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
+        disable_all_buttons()
+
+    elif b3["text"] == "O" and b5["text"] == "O" and b7["text"] == "O":
+        b1.config(bg="green")
+        b4.config(bg="green")
+        b7.config(bg="green")
+        winner = True
+        messagebox.showinfo("Tic tac Toe", "O wins!\nX is a loser!")
+        disable_all_buttons()
+
 
 
 def b_click(b):
@@ -156,10 +212,12 @@ def b_click(b):
         b["text"] = "X"
         clicked = False
         count += 1
+        checkIfWon()
     elif b["text"] == " " and clicked == False:
         b["text"] = "O"
         clicked = True
         count += 1
+        checkIfWon()
     else:
         messagebox.showerror("Tic Tac Toe", "Hey! Someone already chose this box!\n          Pick somewhere else!")
 #Build buttons
@@ -188,6 +246,16 @@ b7.grid(row=2, column=0)
 b8.grid(row=2, column=1)
 b9.grid(row=2, column=2)
 
+# Create Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+#Create Options Menu
+options_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="Options", menu=options_menu)
+options_menu.add_command(label="Reset", command=reset)
+
+reset() 
 
 root.mainloop()
 
